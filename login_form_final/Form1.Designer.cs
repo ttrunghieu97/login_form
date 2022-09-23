1cb4c760ab1eef327a1333d8c08af9ce7c1ea178
tth <tth@ZEPHYUS>
2022-09-23 13:50:33 +0700

    Add project files.

---
+++ login_form_final/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace login_form_final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.user_label = new System.Windows.Forms.Label();
+            this.pass_label = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.user = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Olive;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG NHẬP HỆ THỐNG";
+            // 
+            // user_label
+            // 
+            this.user_label.AutoSize = true;
+            this.user_label.Location = new System.Drawing.Point(206, 103);
+            this.user_label.Name = "user_label";
+            this.user_label.Size = new System.Drawing.Size(76, 16);
+            this.user_label.TabIndex = 1;
+            this.user_label.Text = "User name:";
+            this.user_label.Click += new System.EventHandler(this.user_Click);
+            // 
+            // pass_label
+            // 
+            this.pass_label.AutoSize = true;
+            this.pass_label.Location = new System.Drawing.Point(206, 181);
+            this.pass_label.Name = "pass_label";
+            this.pass_label.Size = new System.Drawing.Size(70, 16);
+            this.pass_label.TabIndex = 1;
+            this.pass_label.Text = "Password:";
+            this.pass_label.Click += new System.EventHandler(this.pass_Click);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(70, 281);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(114, 93);
+            this.login.TabIndex = 2;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(254, 281);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(114, 93);
+            this.exit.TabIndex = 2;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(209, 134);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(213, 22);
+            this.user.TabIndex = 3;
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(209, 220);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(213, 22);
+            this.pass.TabIndex = 3;
+            this.pass.TextChanged += new System.EventHandler(this.passs_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(19, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 153);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.pass_label);
+            this.Controls.Add(this.user_label);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label user_label;
+        private System.Windows.Forms.Label pass_label;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ login_form_final/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_form_final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string tendangnhap = user.Text;
+            string matkhau = pass.Text;
+            string str = "Ten dang nhap cua ban la: " + tendangnhap + "; Mat khau: " + matkhau;
+            MessageBox.Show(str);
+        }
+
+        private void user_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pass_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            DialogResult exit = MessageBox.Show("Ban muon thoat khong?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (exit == DialogResult.Yes)
+            {
+                Application.ExitThread();
+            }
+            else
+            { }
+        }
+        private void passs_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
