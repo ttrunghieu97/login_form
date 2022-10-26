d1ba4fdd5ca321a3a4205ee0ed653b389b3be0dc
ttrunghieu97 <ttrunghieu97@gmail.com>
2022-10-26 14:08:36 +0700

    them btn

---
+++ login_form_final/Form4.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace login_form_final
+{
+    partial class cap_nhat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_masv = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.que_quan = new System.Windows.Forms.TextBox();
+            this.txt_hoten = new System.Windows.Forms.TextBox();
+            this.chk_game = new System.Windows.Forms.CheckBox();
+            this.chk_music = new System.Windows.Forms.CheckBox();
+            this.rbt_nu = new System.Windows.Forms.CheckBox();
+            this.chk_phuot = new System.Windows.Forms.CheckBox();
+            this.chk_thethao = new System.Windows.Forms.CheckBox();
+            this.rbt_nam = new System.Windows.Forms.CheckBox();
+            this.exit = new System.Windows.Forms.Button();
+            this.btn_xoa = new System.Windows.Forms.Button();
+            this.btn_capnhat = new System.Windows.Forms.Button();
+            this.btn_them = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ngay_sinh = new System.Windows.Forms.Label();
+            this.ho_ten = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.masv = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_masv
+            // 
+            this.txt_masv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_masv.Location = new System.Drawing.Point(244, 81);
+            this.txt_masv.Name = "txt_masv";
+            this.txt_masv.Size = new System.Drawing.Size(213, 30);
+            this.txt_masv.TabIndex = 26;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd-mm-yyyy";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(244, 156);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(180, 30);
+            this.dateTimePicker1.TabIndex = 25;
+            // 
+            // que_quan
+            // 
+            this.que_quan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.que_quan.Location = new System.Drawing.Point(244, 228);
+            this.que_quan.Multiline = true;
+            this.que_quan.Name = "que_quan";
+            this.que_quan.Size = new System.Drawing.Size(249, 63);
+            this.que_quan.TabIndex = 24;
+            // 
+            // txt_hoten
+            // 
+            this.txt_hoten.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_hoten.Location = new System.Drawing.Point(244, 118);
+            this.txt_hoten.Name = "txt_hoten";
+            this.txt_hoten.Size = new System.Drawing.Size(180, 30);
+            this.txt_hoten.TabIndex = 23;
+            // 
+            // chk_game
+            // 
+            this.chk_game.AutoSize = true;
+            this.chk_game.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_game.Location = new System.Drawing.Point(337, 339);
+            this.chk_game.Name = "chk_game";
+            this.chk_game.Size = new System.Drawing.Size(87, 29);
+            this.chk_game.TabIndex = 21;
+            this.chk_game.Text = "Game";
+            this.chk_game.UseVisualStyleBackColor = true;
+            // 
+            // chk_music
+            // 
+            this.chk_music.AutoSize = true;
+            this.chk_music.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_music.Location = new System.Drawing.Point(244, 302);
+            this.chk_music.Name = "chk_music";
+            this.chk_music.Size = new System.Drawing.Size(86, 29);
+            this.chk_music.TabIndex = 20;
+            this.chk_music.Text = "Music";
+            this.chk_music.UseVisualStyleBackColor = true;
+            // 
+            // rbt_nu
+            // 
+            this.rbt_nu.AutoSize = true;
+            this.rbt_nu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbt_nu.Location = new System.Drawing.Point(337, 190);
+            this.rbt_nu.Name = "rbt_nu";
+            this.rbt_nu.Size = new System.Drawing.Size(59, 29);
+            this.rbt_nu.TabIndex = 19;
+            this.rbt_nu.Text = "Nữ";
+            this.rbt_nu.UseVisualStyleBackColor = true;
+            // 
+            // chk_phuot
+            // 
+            this.chk_phuot.AutoSize = true;
+            this.chk_phuot.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_phuot.Location = new System.Drawing.Point(244, 340);
+            this.chk_phuot.Name = "chk_phuot";
+            this.chk_phuot.Size = new System.Drawing.Size(85, 29);
+            this.chk_phuot.TabIndex = 18;
+            this.chk_phuot.Text = "Phượt";
+            this.chk_phuot.UseVisualStyleBackColor = true;
+            // 
+            // chk_thethao
+            // 
+            this.chk_thethao.AutoSize = true;
+            this.chk_thethao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_thethao.Location = new System.Drawing.Point(337, 301);
+            this.chk_thethao.Name = "chk_thethao";
+            this.chk_thethao.Size = new System.Drawing.Size(112, 29);
+            this.chk_thethao.TabIndex = 17;
+            this.chk_thethao.Text = "Thể thao";
+            this.chk_thethao.UseVisualStyleBackColor = true;
+            // 
+            // rbt_nam
+            // 
+            this.rbt_nam.AutoSize = true;
+            this.rbt_nam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbt_nam.Location = new System.Drawing.Point(244, 191);
+            this.rbt_nam.Name = "rbt_nam";
+            this.rbt_nam.Size = new System.Drawing.Size(75, 29);
+            this.rbt_nam.TabIndex = 22;
+            this.rbt_nam.Text = "Nam";
+            this.rbt_nam.UseVisualStyleBackColor = true;
+            // 
+            // exit
+            // 
+            this.exit.AutoSize = true;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.Image = global::login_form_final.Properties.Resources.exit;
+            this.exit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.exit.Location = new System.Drawing.Point(524, 258);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(160, 38);
+            this.exit.TabIndex = 16;
+            this.exit.Text = "Thoát";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click_1);
+            // 
+            // btn_xoa
+            // 
+            this.btn_xoa.AutoSize = true;
+            this.btn_xoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_xoa.Image = global::login_form_final.Properties.Resources.delete;
+            this.btn_xoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_xoa.Location = new System.Drawing.Point(524, 209);
+            this.btn_xoa.Name = "btn_xoa";
+            this.btn_xoa.Size = new System.Drawing.Size(160, 38);
+            this.btn_xoa.TabIndex = 15;
+            this.btn_xoa.Text = "Xóa";
+            this.btn_xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_capnhat
+            // 
+            this.btn_capnhat.AutoSize = true;
+            this.btn_capnhat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_capnhat.Image = global::login_form_final.Properties.Resources.update;
+            this.btn_capnhat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_capnhat.Location = new System.Drawing.Point(524, 156);
+            this.btn_capnhat.Name = "btn_capnhat";
+            this.btn_capnhat.Size = new System.Drawing.Size(160, 38);
+            this.btn_capnhat.TabIndex = 14;
+            this.btn_capnhat.Text = "Cập nhật";
+            this.btn_capnhat.UseVisualStyleBackColor = true;
+            // 
+            // btn_them
+            // 
+            this.btn_them.AutoSize = true;
+            this.btn_them.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_them.Image = global::login_form_final.Properties.Resources.sign_add_icon;
+            this.btn_them.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_them.Location = new System.Drawing.Point(524, 102);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(160, 38);
+            this.btn_them.TabIndex = 13;
+            this.btn_them.Text = "Thêm";
+            this.btn_them.UseVisualStyleBackColor = true;
+            this.btn_them.Click += new System.EventHandler(this.btn_them_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(117, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Sở thích";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(116, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Quê quán";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(116, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Giới tính";
+            // 
+            // ngay_sinh
+            // 
+            this.ngay_sinh.AutoSize = true;
+            this.ngay_sinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ngay_sinh.Location = new System.Drawing.Point(116, 156);
+            this.ngay_sinh.Name = "ngay_sinh";
+            this.ngay_sinh.Size = new System.Drawing.Size(105, 25);
+            this.ngay_sinh.TabIndex = 8;
+            this.ngay_sinh.Text = "Ngày sinh:";
+            // 
+            // ho_ten
+            // 
+            this.ho_ten.AutoSize = true;
+            this.ho_ten.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ho_ten.Location = new System.Drawing.Point(117, 121);
+            this.ho_ten.Name = "ho_ten";
+            this.ho_ten.Size = new System.Drawing.Size(95, 25);
+            this.ho_ten.TabIndex = 7;
+            this.ho_ten.Text = "Họ - Tên:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // masv
+            // 
+            this.masv.AutoSize = true;
+            this.masv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masv.Location = new System.Drawing.Point(117, 81);
+            this.masv.Name = "masv";
+            this.masv.Size = new System.Drawing.Size(128, 25);
+            this.masv.TabIndex = 6;
+            this.masv.Text = "Mã sinh viên:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(289, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 38);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Cập nhật DS SV";
+            // 
+            // cap_nhat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_masv);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.que_quan);
+            this.Controls.Add(this.txt_hoten);
+            this.Controls.Add(this.chk_game);
+            this.Controls.Add(this.chk_music);
+            this.Controls.Add(this.rbt_nu);
+            this.Controls.Add(this.chk_phuot);
+            this.Controls.Add(this.chk_thethao);
+            this.Controls.Add(this.rbt_nam);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.btn_xoa);
+            this.Controls.Add(this.btn_capnhat);
+            this.Controls.Add(this.btn_them);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ngay_sinh);
+            this.Controls.Add(this.ho_ten);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.masv);
+            this.Name = "cap_nhat";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.cap_nhat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_masv;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox que_quan;
+        private System.Windows.Forms.TextBox txt_hoten;
+        private System.Windows.Forms.CheckBox chk_game;
+        private System.Windows.Forms.CheckBox chk_music;
+        private System.Windows.Forms.CheckBox rbt_nu;
+        private System.Windows.Forms.CheckBox chk_phuot;
+        private System.Windows.Forms.CheckBox chk_thethao;
+        private System.Windows.Forms.CheckBox rbt_nam;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button btn_xoa;
+        private System.Windows.Forms.Button btn_capnhat;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label ngay_sinh;
+        private System.Windows.Forms.Label ho_ten;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label masv;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ login_form_final/Form4.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace login_form_final
+{
+    public partial class cap_nhat : Form
+    {
+        public cap_nhat()
+        {
+            InitializeComponent();
+        }
+
+        private void cap_nhat_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void masv_text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            //// 
+                
+        }
+
+        private void exit_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_them_Click_1(object sender, EventArgs e)
+        {
+            string masinhvien = txt_masv.Text;
+            string hoten = txt_hoten.Text;
+            string ngaysinh = dateTimePicker1.Text;
+            int gioitinh = 0;
+            if (rbt_nam.Checked == true)
+            {
+                gioitinh = 1;
+            }
+            else if (rbt_nu.Checked == true)
+            {
+                gioitinh = 0;
+            }
+            string quequan = que_quan.Text;
+            string sothich = "";
+            if (chk_game.Checked == true)
+            {
+                sothich = sothich + " ;" + chk_game.Text;
+            }
+            if (chk_phuot.Checked == true)
+            {
+                sothich = sothich + " ;" + chk_phuot.Text;
+            }
+            if (chk_music.Checked == true)
+            {
+                sothich = sothich + " ;" + chk_music.Text;
+            }
+            if (chk_thethao.Checked == true)
+            {
+                sothich = sothich + " ;" + chk_thethao.Text;
+            }
+            string ghichu = "";
+            string strSQL = "";
+            strSQL = strSQL + "  insert into sinhvien(masv, hoten, ngaysinh, gioitinh, ";
+            strSQL = strSQL + " quequan, sothich, ghichu) ";
+            strSQL = strSQL + " values('" + masinhvien + "', N'" + hoten + "', '" + ngaysinh + "', '" + gioitinh + "', ";
+            strSQL = strSQL + "  N'" + quequan + "', N'" + sothich + "', '" + ghichu + "')";
+            connection_DB cn_mh = new connection_DB();
+            SqlConnection cnn_mh = cn_mh.Load_Connection();
+            string x = "";
+            x = cn_mh.Excute_DataTable(strSQL, cnn_mh);
+            if (x == "")
+            {
+                MessageBox.Show("Them thanh cong");
+            }
+            else
+            {
+                MessageBox.Show("Them that bai");
+            }
+        }
+    }
+}
